906af38ff57ddb7091e1fbca9e4fcac88154f46b
Levi <leviszaboo@icloud.com>
2023-09-06 18:53:13 +0200

    fixed import route

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from "react-query";
 
 import { AuthProvider } from "./utils/AuthContext";
 
-import '/Users/leventeszabo/Desktop/website/website.html/luigi-admin-ts/app/globals.css'
+import '@/app/globals.css'
 
 export default function RootLayout({ children }: any) {
   const queryClient = new QueryClient();
